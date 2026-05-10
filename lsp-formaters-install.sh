09fb9032fcdd4001d6a147d8533598b6705d664c
AndrijaSkontra <skontra.andrija@gmail.com>
2026-05-10 13:14:03 +0200

    lsp in install

--- lsp-formaters-install.sh
+++ lsp-formaters-install.sh
@@ -2,4 +2,5 @@ brew install tailwindcss-language-server
 npm install -g @prisma/language-server
 sudo pacman -S astyle
 npm install -g devsense-php-ls
+npm install -g typescript-language-server typescript
 # todo rest and for each system installing
